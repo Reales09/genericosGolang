7e9d3395e361a1dabd781b917fb5b809eeb67a7e
Reales Myles <desarrollador4@azsmart.net>
2023-06-06 15:00:11 -0500

    Parametros de tipos y restricciones

--- main.go
+++ main.go
@@ -8,8 +8,28 @@ func PrintList(list ...any) {
 	}
 }
 
+type integer int
+
+// parametros de tipos
+func Sum[T ~int | ~float64](nums ...T) T {
+	var total T
+	for _, num := range nums {
+		total += num
+	}
+
+	return total
+}
+
 func main() {
 	PrintList("Reales", "Myles")
 	PrintList(123, 456, 789)
 	PrintList("Hola", 456, true)
+
+	var num1 integer = 100
+	var num2 integer = 300
+	// pareametros de tipos
+	fmt.Println(Sum(4, 5, 6, 7))
+	fmt.Println(Sum(4.5, 5.4, 6.8, 7.2))
+	fmt.Println(Sum(num1, num2))
+
 }
