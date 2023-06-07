08e710b51413c8b6686c88c4d5525f95b5ba0a71
Reales Myles <desarrollador4@azsmart.net>
2023-06-07 12:44:29 -0500

    Crear restricciones

--- main.go
+++ main.go
@@ -1,6 +1,10 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+
+	"golang.org/x/exp/constraints"
+)
 
 func PrintList(list ...any) {
 	for _, value := range list {
@@ -10,8 +14,12 @@ func PrintList(list ...any) {
 
 type integer int
 
+type Numbers interface {
+	~int | ~float64 | ~float32 | ~uint
+}
+
 // parametros de tipos
-func Sum[T ~int | ~float64](nums ...T) T {
+func Sum[T constraints.Integer | constraints.Float](nums ...T) T {
 	var total T
 	for _, num := range nums {
 		total += num
@@ -28,8 +36,8 @@ func main() {
 	var num1 integer = 100
 	var num2 integer = 300
 	// pareametros de tipos
-	fmt.Println(Sum(4, 5, 6, 7))
-	fmt.Println(Sum(4.5, 5.4, 6.8, 7.2))
+	fmt.Println(Sum[uint](4, 5, 6, 7))
+	fmt.Println(Sum[float32](4.5, 5.4, 6.8, 7.2))
 	fmt.Println(Sum(num1, num2))
 
 }
