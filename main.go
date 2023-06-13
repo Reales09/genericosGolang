ea9b75ee31bab0d11f468e2c64d37ee27b66b020
Reales Myles <rclarence123@gmail.com>
2023-06-12 20:17:59 -0500

    Resticciones y operadores

--- main.go
+++ main.go
@@ -28,16 +28,49 @@ func Sum[T constraints.Integer | constraints.Float](nums ...T) T {
 	return total
 }
 
-func main() {
-	PrintList("Reales", "Myles")
-	PrintList(123, 456, 789)
-	PrintList("Hola", 456, true)
+func Includes[T comparable](list []T, value T) bool {
+	for _, item := range list {
+		if item == value {
+			return true
+		}
+	}
+	return false
+}
 
-	var num1 integer = 100
-	var num2 integer = 300
-	// pareametros de tipos
-	fmt.Println(Sum[uint](4, 5, 6, 7))
-	fmt.Println(Sum[float32](4.5, 5.4, 6.8, 7.2))
-	fmt.Println(Sum(num1, num2))
+func filter[T constraints.Ordered](list []T, callback func(T) bool) []T {
+	result := make([]T, 0, len(list))
+
+	for _, item := range list {
+
+		if callback(item) {
+			result = append(result, item)
+		}
+	}
+
+	return result
+}
+
+func main() {
+	// PrintList("Reales", "Myles")
+	// PrintList(123, 456, 789)
+	// PrintList("Hola", 456, true)
+
+	// var num1 integer = 100
+	// var num2 integer = 300
+	// // pareametros de tipos
+	// fmt.Println(Sum[uint](4, 5, 6, 7))
+	// fmt.Println(Sum[float32](4.5, 5.4, 6.8, 7.2))
+	// fmt.Println(Sum(num1, num2))
+
+	strings := []string{"a", "b", "c", "d"}
+	numbers := []int{1, 2, 3, 4}
+
+	// fmt.Println(Includes(strings, "a"))
+	// fmt.Println(Includes(strings, "f"))
+	// fmt.Println(Includes(numbers, 4))
+	// fmt.Println(Includes(numbers, 5))
+
+	fmt.Println(filter(numbers, func(value int) bool { return value > 2 }))
+	fmt.Println(filter(strings, func(value string) bool { return value > "b" }))
 
 }
